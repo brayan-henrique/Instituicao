70917f1429aa284af5b80e954d2e07f30c1f6fe9
Brayan Henrique <brayan.henrique200808@gmail.com>
2026-03-24 20:36:33 -0300

    Adicionar arquivos de projeto.

---
+++ Instituição/Instituicao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituição
+{
+    internal class Instituicao
+    {
+        public String Nome { get; set; }
+        public String Endereço { get; set; }
+        public Departamentos[] departamentos { get; set; } =
+            new Departamentos[10];
+
+        int posicaoDepartamento = 0;
+
+        public void GerarDepartamentos(Departamentos d)
+        {
+            if (posicaoDepartamento < 10)
+            {
+                departamentos[posicaoDepartamento++] = d;
+
+            }
+        }
+        public void ListarDepartamentos()
+        {
+            for (int i = 0; i < posicaoDepartamento; i++)
+            {
+                Console.WriteLine(departamentos[i].Nome);
+            }
+        }
+    }
+}
---
+++ Instituição/Program.cs
@@ -0,0 +1,19 @@
+﻿using Instituição;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Coloque o nome do Departamento:");
+            Departamentos d = new Departamentos();
+            d.Nome = Console.ReadLine();
+            Instituicao Itau = new Instituicao();
+            Itau.Nome = "Itau";
+            Itau.Endereço = "Rua do Itau, 123";
+            Itau.GerarDepartamentos(d);
+            Itau.ListarDepartamentos();
+        }
+    }
+}
